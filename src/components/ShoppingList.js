e9f92a5bce8d1286de09825ee4d5a25f53fec62c
Jose Aguilar <jaguilarny@gmail.com>
2022-07-08 10:59:51 -0400

    Lab complete

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -6,7 +6,7 @@ import Item from "./Item";
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
-
+  console.log('new items list', items)
   //useEffect
   useEffect(() => {
     fetch("http://localhost:4000/items")
@@ -18,11 +18,27 @@ function ShoppingList() {
 
   function handleAddItem(newItem) {
     setItems([...items, newItem])
+  };
+
+  function handleUpdateItem(updatedItem) {
+    const updatedItems = items.map(item => {
+      if (item.id === updatedItem.id) {
+        return updatedItem;
+      } else {
+        return item
+      }
+    });
+    setItems(updatedItems)
+  };
+
+  function handleDeleteItem(deletedItem) {
+    const updatedItems = items.filter(item => item.id !== deletedItem.id)
+    setItems(updatedItems)
   }
 
   function handleCategoryChange(category) {
     setSelectedCategory(category);
-  }
+  };
 
   const itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
@@ -39,7 +55,7 @@ function ShoppingList() {
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item key={item.id} item={item} onUpdateItem={handleUpdateItem} onDeleteItem={handleDeleteItem}/>
         ))}
       </ul>
     </div>
